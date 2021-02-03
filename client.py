9dc5c3a8aa42b36329be41d33462e7c86035d927
gigio3010 <gigio3010@users.noreply.github.com>
2021-02-03 13:35:14 +0100

    v2

--- client.py
+++ client.py
@@ -2,12 +2,18 @@
 
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-
-print('Received', data.decode())
+SERVER_ADDRESS = '127.0.0.1'  # The server's hostname or IP address
+SERVER_PORT = 65433        # The port used by the server
+sock_service = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
+dati = input("Inserisci messaggio per il server: ")
+dati = dati.encode()
+# Invia i dati al server
+sock_service.send(dati)
+# Riceve i dati dal server
+dati = sock_service.recv(2048)
+if dati:
+    # Converte i dati dal formato  byte a stringa
+    dati = dati.decode()
+    print("Ho ricevuto dal server: ")
+    print(dati + '\n')
--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 #https://realpython.com/python-sockets/
 import socket
@@ -6,15 +5,27 @@ import socket
 HOST = '127.0.0.1'  # Indirizzo dell'interfaccia standard di loopback (localhost)
 PORT = 65432        # Porta di ascolto, la lista di quelle utilizzabili parte da >1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print("[*] In ascolto su %s:%d" % (HOST, PORT))
-    clientsocket, address = s.accept()
-    with clientsocket as cs:
-        print('Connessione da', address)
-        while True:
-            data = cs.recv(1024)
-            if not data:
-                break
-            cs.sendall(data)
+sock_listen=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Optionale: permette di riavviare subito il codice,
+# altrimenti bisognerebbe aspettare 2-4 minuti prima di poter riutilizzare(bindare) la stessa porta
+sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+sock_listen.bind((HOST, PORT))
+sock_listen.listen()
+print("[*] In ascolto su %s:%d" % (HOST, PORT))
+sock_service, address_client = sock_listen.accept()
+with sock_service as ss:
+    print('Connessione da', address_client)
+    while True:
+        dati = ss.recv(1024)
+        dati.decode()
+        if not dati:
+            break
+        dati = dati.decode()
+        print("Ricevuto '%s' dal client" % dati)
+        dati = "Ciao, " + str(address_client) + ". Ho ricevuto questo: '" + dati + "'"
+        dati = dati.encode()
+        # Invia i dati modificati al client
+        ss.send(dati)
+        print('Inviato al client:', dati)
