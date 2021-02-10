a5b6ae24ef4ac06b80720a93d34664ef750faf3f
gigio3010 <gigio3010@users.noreply.github.com>
2021-02-10 11:25:27 +0100

    l3

--- client.py
+++ client.py
@@ -10,26 +10,29 @@ output_string=input_bytes_encoded.decode()
 print(type(output_string))
 print(output_string)
 
-import socket
+import socket #importazione degli elementi e funzioni socket
 
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
 
-sock_service = socket.socket()
+sock_service = socket.socket()#definizione socket efunzione
 
-sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
+sock_service.connect((SERVER_ADDRESS, SERVER_PORT))#permette di comunicare col server attravero le variabili di indirizzo e porta a livello IPC
 
 print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
-while True:
+while True:#while che gestische le opzioni confrontando il valore scritto con le variabili presenti nei casi di esso
     try:
         dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ")
     except EOFError:
         print("\nOkay. Exit")
         break
-    if not dati:
-        print("Non puoi inviare una stringa vuota!")
-        continue
-    if dati == '0':
+     # if not dati: #esso avvisa che se il set è vuoto di scrivere qualcosa perchè non gli permette di comunicare e permette di rieseguire l'iterazione con il server 
+     #   print("Non puoi inviare una stringa vuota!")
+     #   continue
+    if not dati:#esso avvisa che se il set è vuoto di scrivere, chiude la comunicazione e interrope il funzionamento 
+        print("Server non risponde. Exit")
+        break
+    if dati == '0':#chiude la comunicazione e interrompe il servizio
         print("Chiudo la connessione con il server!")
         break
     
@@ -39,13 +42,13 @@ while True:
 
     dati = sock_service.recv(2048)
 
-    if not dati:
-        print("Server non risponde. Exit")
-        break
+    #if not dati: #dovrebbe permettere di uscire dal collegamento
+    #    print("Server non risponde. Exit")
+     #   break
     
     dati = dati.decode()
 
-    print("Ricevuto dal server:")
+    print("Ricevuto dal server:")#esegue il calcolo di iterazioni con il server come se fosse un contatore
     print(dati + '\n')
 
 sock_service.close()
--- server.py
+++ server.py
@@ -15,27 +15,27 @@ sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
 
 sock_listen.listen(5)
 
-print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT)))
+print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT)))#comunica che il server è attivo con le specifiche di indirizzo e porta
 
 
 while True:
-    sock_service, addr_client = sock_listen.accept()
-    print("\nConnessione ricevuta da " + str(addr_client))
-    print("\nAspetto di ricevere i dati ")
+    sock_service, addr_client = sock_listen.accept()#accettà la richiesta di comunicazione e
+    print("\nConnessione ricevuta da " + str(addr_client))# definisce l'indirizzo del client e che la comunicazione è avvenuta
+    print("\nAspetto di ricevere i dati ")#finchè non riceve segnala che non sono avvenute iterazioni tra client e server
     contConn=0
     while True:
         dati = sock_service.recv(2048)
         contConn+=1
         if not dati:
-            print("Fine dati dal client. Reset")
+            print("Fine dati dal client. Reset")#chiude e resetta cache dai dati inviati in caso di mancanza di dati non inviati
             break
         
-        dati = dati.decode()
-        print("Ricevuto: '%s'" % dati)
+        dati = dati.decode()#decodifica dati in arrivo
+        print("Ricevuto: '%s'" % dati)#output dei dati in arrivo dal client
         if dati=='0':
-            print("Chiudo la connessione con " + str(addr_client))
+            print("Chiudo la connessione con " + str(addr_client))#chiude la comunicazione con il client che comunica la chiusura del canale
             break
-        dati = "Risposta a : " + str(addr_client) + ". Il valore del contatore è : " + str(contConn)
+        dati = "Risposta a : " + str(addr_client) + ". Il valore del contatore è : " + str(contConn)#contatore delle comunicazioni avvenute con il client specifico(guardare indirizzo)
 
         dati = dati.encode()
 
